ff355ca8410fbdfa9b79ee140395a776fcb8129f
Samridh Gaur <samridhgaur2005@gmail.com>
2023-10-19 21:12:46 +0530

    Updated Project links.jsx

--- src/component/projects/Data.jsx
+++ src/component/projects/Data.jsx
@@ -23,7 +23,7 @@ export const Data = [
       title: "Weather APP",
       description:
         "Developed a Weather application project that displays current weather information and allows users to search for weather data of any city .Integrated  the OpenWeather API to fetch and display real-time weather data Implemented user-friendly features such as temperature, wind, speed, humidity, clouds, weather etc",
-        link:"",
+        link:"https://weather-app-using-api-samridh-gaur.vercel.app/",
     },
     {
       id: 3,
@@ -31,7 +31,7 @@ export const Data = [
       title: "Ekart Ecommerce website",
       description:
         "Developed an Ekart E-Commerce website project using ReactJs, Redux and tailwindCss to create a responsive and user-friendly  beautiful interface. Add to cart, Remove to Cart, Checkout functionality Add.User can navigate between cart and home pages",
-        link:"",
+        link:"https://ecart-samridh-gaur.vercel.app/",
     },
     {
       id: 4,
@@ -40,7 +40,7 @@ export const Data = [
       description:
         "Developed a Discord clone landing page using HTML, CSS, and Tailwind CSS. I used Tailwind CSS to create a responsive and visually appealing design that closely resembles the original Discord landing page. Create a Navbar section, Home, hero, footer, etc.",
 
-        link:""
+        link:"https://discord-clone-samridhgaur.vercel.app/"
     },
 
     {
@@ -49,7 +49,7 @@ export const Data = [
       title: "Plan with Samridh",
       description:
        "Created a Plan with Samridh project featuring 9 city cards. Each card includes the city name, journey price, images, and a brief introduction. The Read More and Less feature allows users to expand or collapse the card content. Additionally, there is a Not Interested button to remove city cards if you don’t like it , and a Refresh button to bring back all the city cards after removal .",
-        link: ""
+        link: "https://plan-with-samridh-gaur.vercel.app/"
     },
     {
         id: 6,
@@ -57,7 +57,7 @@ export const Data = [
       
       description: <a href="https://github.com/Samridh-Gaur" target="_blank" className="contact_button">Click here for more<i className="bx bx-right-arrow-alt contact_button-icon"></i>
       </a>,
-      link:""
+      link:"https://github.com/Samridh-Gaur"
 
     },
   ];
